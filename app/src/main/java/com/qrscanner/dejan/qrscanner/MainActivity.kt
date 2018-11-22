22554499f13aa69f86819b40e3217c60975c0089
Dejan Brnic <dejan.brnic@aerdata.com>
2018-11-22 15:15:20 +0100

    Initial commit

---
+++ app/src/main/java/com/qrscanner/dejan/qrscanner/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.qrscanner.dejan.qrscanner
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_scan.setOnClickListener {
+
+            val scanner = IntentIntegrator(this)
+//            scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // allows only QR to be scanned
+            scanner.setBeepEnabled(false) // disables beep
+            scanner.initiateScan() // returns result on ActivityResult method
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+}
